854be393615fcbde9715155a7e70f5edecec4d45
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 13:34:05 +0600

    loap add

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
 import './App.css';
 
 function App() {
-  const name = "Sakila Nasrin Setu"
-  const email = "setu@gmail.com"
-  const phone = "88018XXXXXXXX"
-  const user =(
-    <div>
-      {name}
-      {email}
-      {phone}
-    </div>
-  )
   return (
     <div className="App">
-      {/* <h1>Full Name is =</h1> {name}
-      <h2>Email Address is = {email}</h2>
-      <h3>Phone Number is = {phone}</h3> */}
-      {user}
-      <GetName name="Sakila"/>
-      <GetNameComponent />
+      <Job salary={953000} position="Sr.Software Enginner" company="Meta"/>
+      <Job salary={753000} position="Software Enginner" company="Amazon"/>
+      <Job salary={32000} position="Jr. Software Enginner" company="Google"/>
     </div>
   );
 }
 
-const GetName = (props) =>{
+const Job = (props) =>{
   return (
-    <div>
-      <i>{props.name}</i>
-      <i> Setu</i>
-      
-    </div>
+    <p><i>Position is = {props.position} <br /> Gross Salary = {props.salary}
+    <br/> Company Name is = {props.company}
+    </i></p>
   )
+  
 }
-
-const GetNameComponent = () =>{
-  return <h1>setu@gmail.com</h1>
-}
-
 
 export default App;
