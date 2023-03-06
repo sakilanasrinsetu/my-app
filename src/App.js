4f80168e9688b9a89784575867dfb0b1cf5794da
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 15:04:19 +0600

    condition add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import style from "./App.module.css";
 
 function App() {
   const age =19
-  const isGreen = true;
+  const isGreen = false;
 
   return (
     <div className={style.App}>
       { age >=18 ? <i>Over Age</i> : <i>Under Age</i>}
       <h1 style={{ color: isGreen ? "green" : "red" }}>This Has Color</h1>
+
+      {isGreen && <button>This is A Button</button>}
     </div>
   );
 }
