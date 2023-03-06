71d37246ac6d51f608d20b478b051f64143b7dd3
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 15:39:27 +0600

    list of objects add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@
 import style from "./App.module.css";
 
 function App() {
-  const names = ['a', 'b', 'c', 'd', 'e', 'd']
+  const users = [
+    {name: 'a', age:10},
+    {name: 'b', age:13},
+    {name: 'c', age:40},
+    {name: 'd', age:17},
+    {name: 'e', age:30},
+    {name: 'f', age:133}
+
+]
 
   
   return (
     <div className={style.App}>
-      {names.map((value, key) =>{
-        return <h1> {value} </h1>
+      {users.map((user, key) =>{
+        return <h1> User Name is = {user.name}, and Age is = {user.age} </h1>
 
       })}     
     </div>
