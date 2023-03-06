24245b9a9f2634561a7afb4017cc8194efdf99b0
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 16:21:41 +0600

    incrase function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 // import './App.css';
 import style from "./App.module.css";
-import {User} from './User'
+import {User} from './User';
+import { useState } from "react";
 
 function App() {
-  const users = [
-    {name: 'a', age:10, isChild : true},
-    {name: 'b', age:13, isChild : true},
-    {name: 'c', age:40, isChild : false},
-    {name: 'd', age:17, isChild : true},
-    {name: 'e', age:30, isChild : false},
-    {name: 'f', age:133, isChild : false}
+  // let age = 0;
+  const [age, setAge] = useState(0)
 
-]
-  
+  const inCraseAge = () =>{
+    // age = age +1;
+    // console.log(age)
+    setAge(age+3);
+  };
+
   return (
     <div className={style.App}>
-{/* 
-      {users.map((user, key) =>{
-        // return <User  name = {user.name} age={user.age}/>
-        if (user.isChild) return <i>{user.name} is Child and age is {user.age} <br /> </i>
-        else return <i>...................... <br/>{user.name} is not Child and age is {user.age} <br /> </i>
+      {age} <button onClick={inCraseAge}>Incrase Age</button>
 
-      })} 
-           */}
-
-           {users.map(
-            (user, key) => !user.isChild && <i>{user.name}<br /></i>
-           )}
     </div>
   );
 }
