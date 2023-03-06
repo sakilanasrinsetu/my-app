3192ed9ba388d00487942e6876569cfcfd36a45f
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 16:09:45 +0600

    condition writ with diffrent way

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 // import './App.css';
 import style from "./App.module.css";
+import {User} from './User'
 
 function App() {
   const users = [
-    {name: 'a', age:10},
-    {name: 'b', age:13},
-    {name: 'c', age:40},
-    {name: 'd', age:17},
-    {name: 'e', age:30},
-    {name: 'f', age:133}
+    {name: 'a', age:10, isChild : true},
+    {name: 'b', age:13, isChild : true},
+    {name: 'c', age:40, isChild : false},
+    {name: 'd', age:17, isChild : true},
+    {name: 'e', age:30, isChild : false},
+    {name: 'f', age:133, isChild : false}
 
 ]
-
   
   return (
     <div className={style.App}>
+{/* 
       {users.map((user, key) =>{
-        return <User  name = {user.name} age={user.age}/>
+        // return <User  name = {user.name} age={user.age}/>
+        if (user.isChild) return <i>{user.name} is Child and age is {user.age} <br /> </i>
+        else return <i>...................... <br/>{user.name} is not Child and age is {user.age} <br /> </i>
 
-      })}     
+      })} 
+           */}
+
+           {users.map(
+            (user, key) => !user.isChild && <i>{user.name}<br /></i>
+           )}
     </div>
   );
-}
-const User = (props) =>{
-  return(
-    <div>
-      {props.name} {props.age}
-    </div>
-  )
 }
 
 export default App;
