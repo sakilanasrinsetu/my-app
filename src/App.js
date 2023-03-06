9aed5cc4de0fe0c10b58428777c6cee709be0e33
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 15:03:10 +0600

    color

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import style from "./App.module.css";
 
 function App() {
   const age =19
+  const isGreen = true;
 
   return (
     <div className={style.App}>
       { age >=18 ? <i>Over Age</i> : <i>Under Age</i>}
+      <h1 style={{ color: isGreen ? "green" : "red" }}>This Has Color</h1>
     </div>
   );
 }
