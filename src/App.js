fe6fe483dc9746df6a9f4fbfc0d58b759e11592f
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 13:43:53 +0600

    css file name rename and call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+// import './App.css';
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <Job salary={953000} position="Sr.Software Enginner" company="Meta"/>
       <Job salary={753000} position="Software Enginner" company="Amazon"/>
       <Job salary={32000} position="Jr. Software Enginner" company="Google"/>
@@ -12,7 +13,7 @@ function App() {
 
 const Job = (props) =>{
   return (
-    <p><i>Position is = {props.position} <br /> Gross Salary = {props.salary}
+    <p className={style.Name}><i>Position is = {props.position} <br /> Gross Salary = {props.salary}
     <br/> Company Name is = {props.company}
     </i></p>
   )
