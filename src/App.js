eca5b1bef1a921932de78318bad7858ebf4a8341
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 15:35:41 +0600

    loop add

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import style from "./App.module.css";
 
 function App() {
-  const age =19
-  const isGreen = false;
+  const names = ['a', 'b', 'c', 'd', 'e', 'd']
 
+  
   return (
     <div className={style.App}>
-      { age >=18 ? <i>Over Age</i> : <i>Under Age</i>}
-      <h1 style={{ color: isGreen ? "green" : "red" }}>This Has Color</h1>
+      {names.map((value, key) =>{
+        return <h1> {value} </h1>
 
-      {isGreen && <button>This is A Button</button>}
+      })}     
     </div>
   );
 }
