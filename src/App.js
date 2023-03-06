b089780799ce7f5cf94c1db61d817bdb4bcdf5e7
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-03-06 18:15:53 +0600

    increase and decrease and set 0

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import {User} from './User';
 import { useState } from "react";
 
 function App() {
-  const [textColor, setTextColor]=useState("Green");
+  const [count, setCount] = useState(0);
+  
+  const increase = () => {
+    setCount(count+1);
+  }
+  const decrease = () => {
+    setCount(count-1);
+  }
+
+  const seToZero = () => {
+    setCount(0);
+  }
+
 
   return (
     <div className="App">
-      <button onClick={() =>{
-      setTextColor(textColor === "green" ? "red": "green");
-      }}
-      >
-      Show/Hide
-      </button>
-      <h1 style={{color: textColor}}>Hi and hello</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={seToZero}>Set to Zero</button>
+
+      {count}
+
     </div>
   );
 }
